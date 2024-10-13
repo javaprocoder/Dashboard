346232731311ea0dea50313b57fe47d38d78c1a3
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-10-13 09:47:09 +0530

    Add files via upload

---
+++ Fashion-Store/src/main/java/controller/DashFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashFormController {
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEnterOnAction(ActionEvent event) {
+
+    }
+
+}
